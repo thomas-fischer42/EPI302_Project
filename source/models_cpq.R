fffbbd1e2a742fc3c4837fe46b5eb75ae305a1b6
Thomas Fischer <thomas.fischer2@uzh.ch>
2020-05-28 06:58:45 +0200

    Update

--- source/models_cpq.R
+++ source/models_cpq.R
@@ -1,12 +1,13 @@
-fit_cpq0 <- lm(log(cpq) ~ age65_0 + pa_0 + comorb1_0 +  
-                 gastroad_0 + death + metformin_0 + hrqolcat_0, 
+fit_cpq0 <- lm(log(cpq) ~ age65_0 + pa_0 + comorb1_0 +
+                 hosp10kmdens_0 + metformin_0 + hrqolcat_0, 
                data = sdc_data)
 fit_cpq1 <- update(fit_cpq0, .~. + intgroup_0)
-fit_cpq2 <- update(fit_cpq1, .~. + log(hosp+1))
+fit_cpq2 <- update(fit_cpq1, .~. + hosp)
+fit_cpq3 <- update(fit_cpq1, .~. + log(hosp+1))
+fit_cpq4 <- update(fit_cpq1, .~. + log(hosp+1), data = sdc_data %>% filter(death == "No"))
 
-#texreg(l = list(fit_cpq0, fit_cpq1, fit_cpq2), 
+#texreg(l = list(fit_cpq0, fit_cpq1, fit_cpq2, fit_cpq3, fit_cpq4), 
 #       file = paste0(getwd(),"/tables/fit_cpq.tex"),
 #       digits = 3, label = "tab:fit_cpq")
 
-sdc_data$resid_cpq <- (sdc_data$cpq - exp(fitted(fit_cpq1)) * mean(exp(resid(fit_cpq1))))
-
+#sdc_data$resid_cpq <- (sdc_data$cpq - exp(fitted(fit_cpq1)) * mean(exp(resid(fit_cpq1))))
--- source/models_qaly.R
+++ source/models_qaly.R
@@ -1,6 +1,6 @@
 
 fit_qaly0 <- lm( qaly ~ age65_0 + female_0 + pa_0 + metformin_0 + 
-                   comorb1_0 + hrqol_0, 
+                   hosp10kmdens_0 + comorb1_0 + hrqol_0, 
                  data = sdc_data)
 fit_qaly1 <- update(fit_qaly0, .~. + intgroup_0)
 fit_qaly2 <- update(fit_qaly1, .~. + obstime)
@@ -9,3 +9,4 @@ fit_qaly3 <- update(fit_qaly1, data = sdc_data %>% filter(death == "No"))
 #texreg(l = list(fit_qaly0, fit_qaly1, fit_qaly2, fit_qaly3), 
 #       file = paste0(getwd(),"/tables/fit_qaly.tex"),
 #       digits = 3, label = "tab:fit_qaly")
+
